b3869597c0ab93cd8ad98106a1c60ef9bf964a3b
Maged Milad <maged.milad@blink22.com>
2018-03-07 13:08:01 +0200

    test 1

--- config.rb
+++ config.rb
@@ -19,6 +19,9 @@ set :js_dir, 'javascripts'
 set :images_dir, 'images'
 set :fonts_dir, 'fonts'
 
+# set :build_dir, 'my_build_dir'
+
+
 # Activate the syntax highlighter
 activate :syntax
 ready do
